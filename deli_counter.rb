54753a6fb9d759e3c7b22860e4efa426031bd161
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 05:25:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(arrays)
   if arrays.length >= 1 
     new_array = []
     counter = 1 
-    array.each do |name| 
+    arrays.each do |name| 
       new_array.push("#{counter}. #{name}")
     counter += 1 
   end
