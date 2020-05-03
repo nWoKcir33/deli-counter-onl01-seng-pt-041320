e62b370e592e5ccba41a94a7f37778c02da54acc
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 05:11:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,6 @@ def line(delis)
   if delis.length == 0 
     puts "The line is currently empty"
     elsif delis.length > 2
-    delis[-2]prepend 
+    delis[-2]prepend "The line is currently: "
+    delis.join
    
