d3b88fdd861922202c71e46b3815276b8ba84dfa
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 05:22:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(arrays)
     counter = 1 
     array.each{|name| new_array << "#{counter}. #{name}"
     counter += 1 
+  end
   puts "The line is currently: #{new_array.join(" ")}"
  else
    puts "The line is currently empty."
