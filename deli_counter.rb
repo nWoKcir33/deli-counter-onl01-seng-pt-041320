e223298f4464a8a394f6c6a8471d8a22036cffce
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 05:21:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,5 @@ def line(arrays)
     counter = 1 
     array.each{|name| new_array << "#{counter}. #{name}"
     counter += 1 
-  end
   puts "The line is currently: #{new_array.join(" ")"
-else
    
