ced81a3425157cd377e14d58f47b0d361cdba843
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 05:23:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(arrays)
   if arrays.length >= 1 
     new_array = []
     counter = 1 
-    array.each{|name| new_array << "#{counter}. #{name}"
+    array.each do |name| 
+      new_array << "#{counter}. #{name}"
     counter += 1 
   end
   puts "The line is currently: #{new_array.join(" ")}"
